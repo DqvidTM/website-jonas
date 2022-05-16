54b87b20343ce06cc6f4a8b83a3c1c250c0acb1d
David <d.huhmann34@gmail.com>
2022-05-16 11:57:33 +0200

    publish

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+$.getJSON("https://dcdn.dstn.to/profile/137259014986792960", function (data) {
+  var discordtext = document.getElementById("discordtext");
+
+  discordtext.innerHTML =
+    "<span class='h4 text-white whitney'>" +
+    data.user.username +
+    "</span><span class='h4 text-white-50 whitney'>#" +
+    data.user.discriminator +
+    "</span>";
+
+  document
+    .getElementById("buttoncopystandard")
+    .addEventListener("click", copyToClickBoard);
+  var buttoncopystandard = document.getElementById("buttoncopystandard");
+  var buttoncopydiv = document.getElementById("buttoncopy");
+  var copyText = data.user.username + data.user.discriminator;
+
+  function copyToClickBoard() {
+    navigator.clipboard
+      .writeText(copyText)
+      .then(() => {
+        buttoncopystandard.style.display = "none";
+        buttoncopydiv.innerHTML =
+          "<span class='text-white bi bi-clipboard-check-fill'></span>";
+      })
+      .catch((err) => {
+        console.log("Something went wrong", err);
+        buttoncopystandard.style.display = "none";
+        buttoncopydiv.innerHTML =
+          "<span class='text-white bi bi-clipboard-x-fill'></span>";
+      });
+  }
+});
